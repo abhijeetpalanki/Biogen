e5a3030b10e7f330a08361fd82a9b6edcc2db5f3
abhijeetpalanki <ajpalanki@gmail.com>
2024-06-09 13:27:15 -0500

    added magic ui components for bg and hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,31 @@
+import AnimatedGradientText from "@/components/magicui/animated-gradient-text";
+import { ChevronRight, Star } from "lucide-react";
+import Link from "next/link";
+
 export default function Home() {
-  return <main className="">Biogen</main>;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="group w-full flex flex-col items-center justify-center space-y-4 mb-4 text-center">
+        <Link
+          href="https://github.com/abhijeetpalanki/Biogen"
+          target="_blank"
+          className=""
+        >
+          <AnimatedGradientText className="px-6 py-2 rounded-full">
+            <Star className="w-6 h-6 fill-yellow-300 text-yellow-400" />
+            <hr className="mx-2 h-4 w-px bg-gray-300" />
+            Star on GitHub
+            <ChevronRight className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
+          </AnimatedGradientText>
+        </Link>
+        <h1 className="font-extrabold uppercase text-7xl text-center w-full lg:w-[90%] mx-auto pt-4">
+          Craft the perfect bio in seconds!
+        </h1>
+        <p className="text-lg text-accent">
+          Just answer a few questions, and we'll generate a bio that captures
+          who you are.
+        </p>
+      </div>
+    </main>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,13 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -18,6 +18,9 @@ const config = {
       },
     },
     extend: {
+      fontFamily: {
+        sans: ["var(--font-geist-sans)"],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -59,6 +62,11 @@ const config = {
         sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
+        gradient: {
+          to: {
+            backgroundPosition: "var(--bg-size) 0",
+          },
+        },
         "accordion-down": {
           from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
@@ -69,12 +77,13 @@ const config = {
         },
       },
       animation: {
+        gradient: "gradient 8s linear infinite",
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
